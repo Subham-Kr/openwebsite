db6406d061b12ad6b674b3d79f634a22cdb64bdc
rishabhsuyal <rishabhsuyal2001@gmail.com>
2020-07-23 17:43:50 +0530

    new navbar for reviews & download cover changed

--- js/main.js
+++ js/main.js
@@ -113,3 +113,12 @@ function answerc () {
 	}
 	
 }	
+function openNav() {
+	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("main").style.marginLeft= "0";
+  }
